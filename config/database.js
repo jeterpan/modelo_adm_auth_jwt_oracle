9fb47a4b84fc1ea6255a76bfd4da576ad2d15928
JeterPan <41131222+jeterpan@users.noreply.github.com>
2020-08-03 09:10:30 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     hrPool: {
-        user: process.env.WS_ADM_JWT_ORA_USUARIO,
-        password: process.env.WS_ADM_JWT_ORA_SENHA,
-        connectionString: process.env.WS_ADM_JWT_ORA_CONN_STRING,
+        user: process.env.WS_JWT_ADM_ORA_USUARIO,
+        password: process.env.WS_JWT_ADM_ORA_SENHA,
+        connectionString: process.env.WS_JWT_ADM_ORA_CONN_STRING,
         poolMin: 10,
         poolMax: 10,
         poolIncrement: 0
